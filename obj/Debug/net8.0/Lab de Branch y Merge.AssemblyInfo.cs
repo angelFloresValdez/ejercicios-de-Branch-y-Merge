d1d478391a4cc8a1c8799f5e4b4e84ad5febd05d
angel flores <al44546@un.edu.mx>
2024-06-22 18:58:52 -0600

    Guardando cambios antes de cambiar de rama

--- obj/Debug/net8.0/Lab de Branch y Merge.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lab de Branch y Merge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab de Branch y Merge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589740bedaa98e50e5fb6d42bfb584f63888de14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e638ebffb962df482fe7063d625673157c580d12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab de Branch y Merge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab de Branch y Merge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
